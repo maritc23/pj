6c638b16c087e856648477ab6f7c373568e00813
maritc23 <maritc23@gmail.com>
2022-04-13 16:20:41 -0500

    product-journey

---
+++ product-journey/product-journey/src/main/java/com/scotiabank/productjourney/ProductJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.scotiabank.productjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductJourneyApplication.class, args);
+	}
+
+}
---
+++ product-journey/product-journey/src/test/java/com/scotiabank/productjourney/ProductJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scotiabank.productjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
